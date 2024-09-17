a3f66cf435154829d27f94773bfa65688319d2cc
montrell43 <tmerriweather@perseverenow.org>
2024-09-17 14:40:29 -0500

    added new folder to testrepo

---
+++ speedAnalysis/speed_analysis.js
@@ -0,0 +1,16 @@
+let testText = "The quick brown fox jumps over the lazy dog.";
+let startTime, endTime;
+
+function startTest() {
+    // Set the test text
+    document.getElementById("inputText").value = testText;
+    
+    // Reset results and timer
+    document.getElementById("output").innerHTML = "";
+    startTime = new Date().getTime();
+    
+    // Change button text and functionality
+    var button = document.getElementById("btn");
+    button.innerHTML = "End Test";
+    button.onclick = endTest;
+            }
